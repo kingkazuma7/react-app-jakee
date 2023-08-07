3d8d179a57173e93b6df3d895d38c7f3fcd05b99
kingkazuma7 <your.nasionasio7@gmail.com>
2023-08-07 09:42:41 +0900

    カウントup down

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useState } from 'react';
 import './App.css' ;
 
 function App() {
+  console.log("app");
+  const [count, setCount] = useState(0);
+  
+  const onClickCountUp = () => {
+    setCount(count + 1);
+    console.log("setcount");
+  }
+  
+  const onClickCountDown = () => {
+    setCount(count - 1);
+    console.log("setcount");
+  }
+  
   return (
     <div className="App">
-      hoge
+      <p>{count}</p>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <button onClick={onClickCountDown}>カウントダウン</button>
     </div>
   );
 }
